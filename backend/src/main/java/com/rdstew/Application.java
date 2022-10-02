a7753bab5c6b8581d48956aa29ac0da63d4ab816
Ryan Stewart <rdstew11@gmail.com>
2022-10-02 18:21:37 -0400

    Updating Auth to include state

--- backend/src/main/java/com/rdstew/Application.java
+++ backend/src/main/java/com/rdstew/Application.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class Application{
 
+    public static String backend_domain = "http://localhost:8080";
+    public static String frontend_domain = "http://localhost:4200";
+
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
     }
--- backend/src/main/java/com/rdstew/controllers/RequestAuthController.java
+++ backend/src/main/java/com/rdstew/controllers/RequestAuthController.java
@@ -4,8 +4,10 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.Random;
 
 import com.rdstew.exceptions.InternalServerError;
+import com.rdstew.Application;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,27 +33,43 @@ public class RequestAuthController {
             throw new InternalServerError();
         }
 
-
         client_id = spotifyConf.getProperty("client.id");
         client_secret = spotifyConf.getProperty("client.secret");
-        String redirect_uri = "http://localhost:8080/api/get-user-token";
+        String redirect_uri = Application.backend_domain + "/api/get-spotify-token";
         String response_type = "code";
+        String state_id = this.generateStateId();
 
         attributes.addAttribute("client_id", client_id);
         attributes.addAttribute("redirect_uri", redirect_uri);
         attributes.addAttribute("response_type", response_type);
+        attributes.addAttribute("state", state_id);
         return new RedirectView("https://accounts.spotify.com/authorize");
     }
 
-    @GetMapping("/api/get-user-token")
+    @GetMapping("/api/get-spotify-token")
     public RedirectView getUserToken(RedirectAttributes attributes){
         
-        return new RedirectView("http://localhost:8080/hello");
+
+        return new RedirectView(Application.frontend_domain + "/home");
     }
 
     @GetMapping("/hello")
     public String getHello(){
         return("Hello");
+    }
+
+    private String generateStateId(){
+        
+        int left_limit = 48;
+        int right_limit = 122;
+        int length = 16;
+        Random random = new Random();
+        String state_id = random.ints(left_limit, right_limit + 1)
+            .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i>= 97))
+            .limit(length)
+            .collecter(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+            .toString();
+        return state_id;
     }
 
 
